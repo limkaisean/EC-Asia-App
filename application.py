13a4abfebf29fd34847d4611ee1bcf6ea938d0b0
arsalan <arsalanc.v2@gmail.com>
2020-07-27 09:27:41 +0800

    Add time support

--- application.py
+++ application.py
@@ -1,14 +1,17 @@
 from flask import Flask, request
 from flask_socketio import SocketIO
 
-import time
+import pytz
+from datetime import datetime
+
+tz = pytz.timezone("Asia/Singapore")
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
 
-# LIMITATION: there can only be one session per meeting room
-# TODO: change request.sid to the meeting room number
+# ASSUMPTION: there can only be one session per meeting room
+# TODO: update meeting room socket on connecting
 
 STATUS_RECEIVED = 'received'
 STATUS_PREPARING = 'preparing'
@@ -47,7 +50,7 @@ def message_handler(message):
             barista_devices.append(request.sid)
         
         oId = message['id']
-        meeting_room = message['meetingRoom']
+        meeting_room = str(message['meetingRoom'])
         status = message['status']
 
         if status == STATUS_COMPLETED:
@@ -76,10 +79,12 @@ def barista_orders(message):
 
 @socketio.on('confirmed_orders_request')
 def confirmed_orders(message):
-    meeting_room = message['meetingRoom']
-    if meeting_room in customer_devices_sockets:
-        sId = customer_devices_sockets[meeting_room]
-        socketio.emit('confirmed_orders_response', customer_devices_orders[meeting_room], room=sId)
+    try:
+        meeting_room = str(message['meetingRoom'])
+        customer_devices_sockets[meeting_room] = request.sid
+        socketio.emit('confirmed_orders_response', customer_devices_orders[meeting_room])
+    except Exception as e:
+        print('Error orders req', e)
 
 @socketio.on('order_request')
 def order(message):
@@ -87,14 +92,14 @@ def order(message):
         order = {}
         order['beverages'] = message['beverages']
         order['status'] = STATUS_RECEIVED
-        order['time'] = 2 # TODO
-        customer_devices_sockets[5] = request.sid
+
+        # get Singapore time in 'hh:mm period' format
+        order['time'] = tz.localize(datetime.now(), is_dst=None).strftime('%l:%M %p')
         global id
         order['id'] = id
         id += 1
         
-        meeting_room = message['meetingRoom']
-        print(meeting_room)
+        meeting_room = str(message['meetingRoom'])
         order['meetingRoom'] = meeting_room
 
         # add order to customer_devices_orders
@@ -108,8 +113,11 @@ def order(message):
         # send confirmation and orders to customer device
         socketio.emit('order_response', {'isSuccessful': True, 'order': order})
         # send order to barista devices
+
+        all_orders = customer_devices_orders.values()
+        all_orders_flat = {oId: room_orders[oId] for room_orders in all_orders for oId in room_orders}
         for bid in barista_devices:
-            socketio.emit('barista_order_relay', order, room=bid)
+            socketio.emit('barista_orders_response', all_orders_flat, room=bid)
     except Exception as e:
         print('Error:', e)
 
